36fd5c51582c010bf3c445088672dad008a10511
Makar A. Pestrikov <mpxx088@gmail.com>
2024-09-25 14:22:39 +0300

    init

---
+++ src/main.rs
@@ -0,0 +1,44 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(pilot_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+use pilot_os::println;
+use core::panic::PanicInfo;
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("Hello World{}", "!");
+
+    pilot_os::init(); // new
+
+    // invoke a breakpoint exception
+    x86_64::instructions::interrupts::int3(); // new
+
+    #[cfg(test)]
+    test_main();
+
+    println!("no crash!!");
+
+    loop {}
+}
+
+/// This function is called on panic.
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    pilot_os::test_panic_handler(info)
+}
+
+#[test_case]
+fn trivial_assertion() {
+    assert_eq!(1, 1);
+}
